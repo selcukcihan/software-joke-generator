66c9a169dd7ec0cd7c49e286d953cd2f31560557
Selcuk Cihan <selcukcihan@gmail.com>
2023-04-14 10:10:24 +0300

    Use previous jokes as input

--- src/business/joke-generator.ts
+++ src/business/joke-generator.ts
@@ -1,5 +1,5 @@
 import { Inject, Service } from 'typedi'
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { OpenAIApi } from 'openai'
 const text2Svg = require('text-svg')
 
@@ -12,16 +12,39 @@ export class JokeGenerator {
   ) {}
 
   async generate() {
-    const input = {
+    const existingResponse = await this.s3Client.send(new GetObjectCommand({
+      Key: `joke.json`,
+      Bucket: this.s3Bucket,
+    }))
+    let existing = JSON.parse(await existingResponse.Body?.transformToString() || '[]')
+
+    let input = {
       model: "gpt-3.5-turbo",
       messages: [
         {role: "user", content: "Make a joke about software engineering."},
       ]
     } as any
+    if (existing.length > 0) {
+      if (existing.length > 20) {
+        existing = existing.slice(20)
+      }
+      const examples = existing.map((e: any) => `- ${e}\n`).join('')
+      input.messages.push(
+        {role: "user", content: `Some examples:\n${examples}`}
+      )
+    }
     console.log(JSON.stringify(input, null, 2))
 
     const response = await this.openai.createChatCompletion(input)
     const generated = response.data.choices[0].message?.content || ''
+
+    existing.push(generated)
+    await this.s3Client.send(new PutObjectCommand({
+      Body: JSON.stringify(existing),
+      Key: `joke.json`,
+      Bucket: this.s3Bucket,
+      ContentType: "application/json",
+    }))
 
     console.log('Generated response: ' + generated)
     const svg = text2Svg(generated)
