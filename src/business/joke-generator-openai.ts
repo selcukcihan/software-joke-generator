ce1c05f7aa0420272d0695da1e5c0fb812848086
Selcuk Cihan <selcukcihan@gmail.com>
2024-05-12 00:42:52 +0300

    Switch to google ai

--- src/business/joke-generator-openai.ts
+++ src/business/joke-generator-openai.ts
@@ -1,6 +1,6 @@
 import { Inject, Service } from 'typedi'
 import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai'
+import { OpenAIApi } from 'openai'
 const text2Svg = require('text-svg')
 
 @Service()
@@ -8,7 +8,7 @@ export class JokeGenerator {
   constructor(
     @Inject('S3_CLIENT') private readonly s3Client: S3Client,
     @Inject('S3_BUCKET') private readonly s3Bucket: string,
-    @Inject('GOOGLE_AI_CLIENT') private readonly ai: GoogleGenerativeAI,
+    @Inject('OPEN_AI_CLIENT') private readonly openai: OpenAIApi,
   ) {}
 
   async generate() {
@@ -18,55 +18,25 @@ export class JokeGenerator {
     }))
     let existing = JSON.parse(await existingResponse.Body?.transformToString() || '[]')
 
-    const model = this.ai.getGenerativeModel({ model: 'gemini-1.5-pro-latest' })
-  
-    const generationConfig = {
-      temperature: 1,
-      topK: 0,
-      topP: 0.95,
-      maxOutputTokens: 8192,
-    }
-  
-    const safetySettings = [
-      {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-      {
-        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-      },
-    ]
-  
-    const chat = model.startChat({
-      generationConfig,
-      safetySettings,
-      history: [
-      ],
-    })
-
-    let input = "Make a joke about software engineering."
+    let input = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {role: "user", content: "Make a joke about software engineering."},
+      ]
+    } as any
     if (existing.length > 0) {
       if (existing.length > 20) {
         existing = existing.slice(20)
       }
       const examples = existing.map((e: any) => `- ${e}\n`).join('')
-      input += `\nSome examples:\n${examples}`
+      input.messages.push(
+        {role: "user", content: `Some examples:\n${examples}`}
+      )
     }
     console.log(JSON.stringify(input, null, 2))
 
-    const result = await chat.sendMessage(input)
-    const response = result.response
-    const generated = response.text() || ''
-    console.log(generated)
+    const response = await this.openai.createChatCompletion(input)
+    const generated = response.data.choices[0].message?.content || ''
 
     existing.push(generated)
     await this.s3Client.send(new PutObjectCommand({
@@ -76,6 +46,7 @@ export class JokeGenerator {
       ContentType: "application/json",
     }))
 
+    console.log('Generated response: ' + generated)
     const svg = text2Svg(generated)
 
     await this.s3Client.send(new PutObjectCommand({
--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -4,6 +4,7 @@ import { Container } from 'typedi'
 import { S3Client } from '@aws-sdk/client-s3'
 import { JokeGenerator } from '../business/joke-generator'
 import { Configuration, OpenAIApi } from 'openai'
+import { GoogleGenerativeAI } from '@google/generative-ai'
 
 
 Container.set('S3_CLIENT', new S3Client({ region: 'eu-west-1' }))
@@ -15,6 +16,7 @@ async function handler(event: any, context: any) {
   Container.set('OPEN_AI_CLIENT', new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY as string,
   })))
+  Container.set('GOOGLE_AI_CLIENT', new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string))
 
   const jokeGenerator = Container.get(JokeGenerator)
   const response = await jokeGenerator.generate()
